8edf0ec17c2e1e9b6bdd169ca210825d59866c10
fuhao.xu <fuhao.xu@yitu-inc.com>
2020-12-11 15:04:41 +0800

    feat: happy init command

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export default {
+  pkgTypes: ['es', 'ts', 'react']
+}
--- src/init.js
+++ src/init.js
@@ -1,5 +1,124 @@
-// import execa from 'execa';
-// execa.command('npm init', { stdio: 'inherit' })//
-export default (type) => {
-  console.log(type)
+import execa from 'execa'
+import assert from 'assert'
+import path from 'path'
+import merge from 'lodash.merge'
+import fs from 'fs-extra'
+import glob from 'glob'
+import ora from 'ora'
+import prompts from 'prompts'
+import config from './config'
+
+const rootDir = process.cwd()
+const bplTmpPath = path.join(rootDir, 'happy-init/boilerplate')
+const commonFilePath = path.join(bplTmpPath, 'common')
+const extendFilePath = path.join(bplTmpPath, 'extend')
+
+function validateArgs(type) {
+  assert(config.pkgTypes.includes(type), 'unknown package type, only support es,ts,react')
+}
+
+// origin package info
+function getOriginInfo() {
+  const parent = path.join(rootDir, '../')
+  const dirName = path.relative(parent, rootDir)
+
+  return {
+    name: dirName,
+    version: '1.0.0',
+    description: '',
+    author: '',
+    license: 'ISC',
+  }
+}
+
+// handle input info
+async function handleInput(origin) {
+  return prompts(
+    Object.keys(origin).map((key) => ({
+      type: 'text',
+      name: key,
+      message: key,
+      initial: origin[key],
+    }))
+  )
+}
+
+// download boilerplate
+function downloadBpl() {
+  return execa.command(`git clone https://github.com/buyan302/happy-init.git`)
+}
+
+// copy common to root
+async function copyCommonFiles(type) {
+  // copy and replace
+  await execa.command(`cp -a ${commonFilePath}/. ${rootDir}`)
+  return execa.command(`cp -a ${bplTmpPath}/${type}/. ${rootDir}`)
+}
+
+// copy extend files
+function copyExtendFiles(inputValues) {
+  const files = glob.sync('**', { cwd: extendFilePath, dot: true })
+
+  return Promise.all(
+    files.map((file) => {
+      const filePath = path.join(extendFilePath, file)
+      const rootFilePath = path.join(rootDir, file)
+      // extend file content
+      if (fs.existsSync(rootFilePath)) {
+        const json1 = fs.readJsonSync(filePath)
+        const json2 = fs.readJsonSync(rootFilePath)
+        return fs.writeJson(rootFilePath, merge({}, json1, json2, inputValues), { spaces: 2 })
+      }
+
+      // copy file to root directly
+      return execa.command(`cp ${filePath} ${rootDir}`)
+    })
+  )
+}
+
+// clear invalid files
+function clearFiles() {
+  return execa.command(`rm -rf happy-init`)
+}
+
+// rename _pkg.json
+function renamePkgJson() {
+  return execa.command(`mv _pkg.json package.json`)
+}
+
+// npm install
+function npmInstall() {
+  return execa.command(`npm install`, {stdio: 'inherit'})
+}
+
+// error-handler
+function handleError(e) {
+  console.error('\x1b[31m%s\x1b[0m', `happy-init: ${e.message}`)
+  process.exit(1)
+}
+
+export default async (type) => {
+  const progress = ora()
+  try {
+    // preprocess
+    validateArgs(type)
+    const origin = getOriginInfo()
+    const inputValues = await handleInput(origin)
+
+    // download process
+    progress.start(`downloading ${type} package from git...`)
+    await downloadBpl(type)
+    await copyCommonFiles(type)
+    await copyExtendFiles(inputValues)
+    await clearFiles()
+    await renamePkgJson()
+    progress.succeed(`downloading compelete, start installing...\n`)
+
+    // install process
+    await npmInstall()
+    progress.succeed(`installing compelete, happy coding!`)
+  } catch (e) {
+    progress.stop()
+    handleError(e)
+  }
 }
